bec5e0967d157beb66926be9eff5fb0630438606
Akash Gite <giteakash97@gmail.com>
2023-01-30 14:00:17 +0530

    rightbar ui

--- src/components/rightBar/rightBar.js
+++ src/components/rightBar/rightBar.js
@@ -1,13 +1,43 @@
-import React from 'react'
-import Notification from '../notification/notification'
-import './rightBar.scss'
+import React from "react";
+import { useState,useEffect } from 'react';
+import axios from 'axios'
+import "./rightBar.scss";
+import SideBarProfile from "../sideBar/sideBarProfile/sideBarProfile";
 
 const RightBar = () => {
-  return (
-    <div className='rightBar'>
-    <Notification />
-    </div>
-  )
+
+  const[users, setUsers] = useState([]);
+
+const getAllUsers = async () =>{
+  const usersData = await axios.get("https://dummyjson.com/users")
+  setUsers(usersData?.data?.users)
 }
 
-export default RightBar
+useEffect(()=>{
+  getAllUsers()
+},[])
+
+
+  return (
+    <div className="rightBar">
+      <div className="rightbarTop">
+        <div className="notifyDaily">
+          <img src="/assets/gift.png" alt="notifyDaily" className="giftImg" />
+          <span className="notifyText">
+            <strong>Akash Gite</strong> and <strong>3 other friends</strong>
+            <br /> have a birthday today.
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="adImg" className="adImg" />
+      </div>
+      <div className="rightbarBottom">
+      <h4>Online friends</h4>
+      {users.map((user)=>(
+        <SideBarProfile user={user} isOnline/>
+      ))}
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
--- src/components/sideBar/sideBarProfile/sideBarProfile.js
+++ src/components/sideBar/sideBarProfile/sideBarProfile.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./sideBarProfile.scss";
 
-const SideBarProfile = ({ user }) => {
+const SideBarProfile = ({ user, isOnline }) => {
   return (
     <div className="sideBarProfile">
       <img src={user.image} alt="profileImg" className="profileImage" />
+      {isOnline && <span className="onlineStatus"></span>}
       <h3>
+    
         {user.firstName} {user.lastName}
       </h3>
     </div>
